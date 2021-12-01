305d489a834e4ecf2a765df2e798a030c7db00ea
Vignesh Pandiarajan <vpandiarajan20@gmail.com>
2021-11-30 21:21:11 -0500

    push

--- Attempt1.py
+++ Attempt1.py
@@ -2,6 +2,10 @@ import torch
 from torch import nn, Tensor
 import torchvision.models as models
 from typing import Dict, Iterable, Callable
+from torchvision import transforms
+from PIL import Image
+import os
+import numpy as np
 
 class FeatureExtractor(nn.Module):
     def __init__(self, model: nn.Module, layers: Iterable[str]):
@@ -26,11 +30,29 @@ class FeatureExtractor(nn.Module):
 
 
 def main():
+    # inception_v3 = models.inception_v3(pretrained=True)
+    # print(inception_v3)
+    # resnet_features = FeatureExtractor(inception_v3, layers=["Mixed_5b"])
+    # dummy_input = torch.ones(1, 3, 224, 224)
+    # features = resnet_features(dummy_input)
+    # print(features)
     googlenet = models.googlenet(pretrained=True)
-    resnet_features = FeatureExtractor(googlenet, layers=["inception5a"])
-    dummy_input = torch.ones(1, 3, 224, 224)
-    features = resnet_features(dummy_input)
-    print(features)
+    resnet_features = FeatureExtractor(googlenet, layers=["inception5b"])
+    activations = np.array([])
+    folder_names = np.array(["Dotted", "RandomImages", "Striped"])
+    for folder in folder_names:    
+        listing = os.listdir(folder)    
+        for file in listing:
+            img = Image.open(folder + "/" + file)
+            convert_tensor = transforms.ToTensor()
+            dummy_input = convert_tensor(img)
+            dummy_input = dummy_input[None, :, :, :]
+            features = resnet_features(dummy_input)
+            newActs = torch.flatten(features["inception5b"])
+            newActs = newActs.detach().numpy()
+            newActs = np.expand_dims(newActs, axis=0)
+            activations = np.append(activations, newActs, axis=0)
+    print(activations.shape)
 
 
 
