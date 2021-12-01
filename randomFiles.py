d99a0f1105f578adc3c9eb659d5cb2989744ac6d
Vignesh Pandiarajan <vpandiarajan20@gmail.com>
2021-11-30 23:37:08 -0500

    small improve

--- randomFiles.py
+++ randomFiles.py
@@ -5,25 +5,30 @@ from shutil import copy
 import pandas as pd
 
 imgDF = pd.read_csv("C:/Users/vpand/Documents/College/Fall 2021/NetDissect/dataset/broden1_224/index.csv")
+imgDF.texture = imgDF.texture.fillna("-1")
+imgDF.texture = imgDF.texture.astype(str)
 
 def getRandomImages():
     randomPaths = imgDF.sample(500)
     for index, img in randomPaths.iterrows():
-        path = "C:/Users/vpand/Documents/College/Fall 2021/NetDissect/dataset/broden1_224/images/" + img['image']
-        print(path)
-        copy(path, "./RandomImages")
+        if ("295" in img.texture or "276" in img.texture or "356" in img.texture): # this is hardcoded, it needs to be better
+            print("one of the wanted textures")
+        else:
+            path = "C:/Users/vpand/Documents/College/Fall 2021/NetDissect/dataset/broden1_224/images/" + img['image']
+            print(path)
+            copy(path, "./RandomImages")
 
 def getTextures(x, pathname):
     # x is the numebr of the texture you want to get
-    print(imgDF.dtypes)
-    imgDF.texture = imgDF.texture.fillna("-1")
-    imgDF.texture = imgDF.texture.astype(str)
     print(imgDF.head())
     rows = imgDF[imgDF.texture.str.contains(x)]
     for index, img in rows.iterrows():
         path = "C:/Users/vpand/Documents/College/Fall 2021/NetDissect/dataset/broden1_224/images/" + img['image']
         print(path)
-        copy(path, "./" + pathname)
+        try:
+            copy(path, "./" + pathname)
+        except:
+            continue
 
 
 def main():
