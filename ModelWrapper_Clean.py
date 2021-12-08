a6c2eb2e2c5d4eb627c7ec82f62d21f20d00441a
Vignesh Pandiarajan <vpandiarajan20@gmail.com>
2021-12-07 22:03:13 -0500

    Update ModelWrapper_Clean.py

--- ModelWrapper_Clean.py
+++ ModelWrapper_Clean.py
@@ -5,4 +5,30 @@ class ModelWrapper_Clean():
         super().__init__()
         self.model = model
         self.layers = layers
-        self.activations = {layer: torch.empty(0) for layer in layers}
+        self.activations = {}
+        self.gradients
+        for layer in layers:
+            layer_hook = dict([*self.model.named_modules()])[layer]
+            layer_hook.register_forward_hook(self.save_features(layer))
+    
+    def save_features(self, layer):
+        def hook_to_register(_, __, output):
+            self.activations[layer] = output
+        return hook_to_register
+    
+    def save_gradient(self, grad):
+        self.gradients = grad
+        
+    def get_gradients(self, classNumber, layer):
+        activation = self.activations[layer]
+        activation.register_hook(self.save_gradient)
+        logit = self.output[:, classNumber]
+        logit.backward(torch.ones_like(logit), retain_graph=True)
+        gradients = self.gradients.cpu().detach().numpy()
+        return gradients
+
+    def forward(self, x):
+        self.output = self.model(x)
+        return self._features
+
+
